1200305e90e2175d3ebbb5f9fe5f8e7819861031
half-shell <michel.maubert.pro@gmail.com>
2021-05-04 15:30:34 +0200

    Update tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -8,7 +8,7 @@ const getFilePath = (filename) => {
 };
 
 const getInputOutputFiles = () => (
-    [1, 2, 3, /*4, 5*/].map(i => [
+    [1, 2, 3, 4, 5].map(i => [
         getFilePath(util.format('%s%s.txt', "input", i)),
         getFilePath(util.format('%s%s.txt', "output", i)),
     ])
@@ -22,4 +22,16 @@ describe('wecal', () => {
 
         expect(findSlot(input)).toMatch(output);
     });
+
+    it('throws if no slot is available', () => {
+        const input = [
+            '1 08:00-17:59',
+            '2 08:00-17:59',
+            '3 08:00-17:59',
+            '4 08:00-17:59',
+            '5 08:00-17:59'
+        ].reduce((acc, s) => `${acc}\n${s}`, '').trim();
+
+        expect(() => findSlot(input)).toThrowError(Error);
+    })
 });
